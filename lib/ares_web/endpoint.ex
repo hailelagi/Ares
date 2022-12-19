bb11a7ca53f132e34a8845823f622e5c5b67a68c
Haile Lagi <52631736+hailelagi@users.noreply.github.com>
2022-12-19 01:45:25 +0100

    feat(wip): socket protocol boilerplate

--- lib/ares_web/endpoint.ex
+++ lib/ares_web/endpoint.ex
@@ -11,6 +11,7 @@ defmodule AresWeb.Endpoint do
   ]
 
   socket "/live", Phoenix.LiveView.Socket, websocket: [connect_info: [session: @session_options]]
+  socket "/reconcile-log", AresWeb.Websockets.LogSocket, websocket: true
 
   # Serve at "/" the static files from "priv/static" directory.
   #
---
+++ lib/ares_web/websockets/log_socket.ex
@@ -0,0 +1,39 @@
+defmodule AresWeb.Websockets.LogSocket do
+  @moduledoc """
+    `Phoenix.Socket.Transport implementation of a binary protocol for
+    exchanging binary-encoded server logs between two servers`
+  """
+
+  @behaviour Phoenix.Socket.Transport
+
+  require Logger
+
+  @impl true
+  def child_spec(opts) do
+    endpoint = Keyword.fetch!(opts, :endpoint)
+    partitions = Keyword.get(opts, :partitions, System.schedulers_online())
+
+    Supervisor.child_spec({Phoenix.Socket.PoolSupervisor, {endpoint, __MODULE__, partitions}},
+      id: __MODULE__
+    )
+  end
+
+  def connect(transport_info) do
+    {:ok, transport_info}
+  end
+
+  def init(state) do
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_in({_text, _opts}, state) do
+    Logger.warn("recived msg okay!")
+    {:ok, state}
+  end
+
+  @impl true
+  def terminate(_reason, %{user_id: id}) do
+    :ok
+  end
+end
